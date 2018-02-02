7b84a920d541bbb3af7ee33f8a46f6f60e47e6ed
Emeka <emekaosuagwuandela@gmail.com>
2018-02-02 10:15:04 +0100

    add SubscriptionSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(CategorySeeder::class);
         // $this->call(DevotionSeeder::class);
         $this->call(AdminUserSeeder::class);
+        $this->call(SubscriptionSeeder::class);
     }
 }
