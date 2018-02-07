ba99f20b798d39c1091e7b53d6ab199dcb3b52e2
Emeka <emekaosuagwuandela@gmail.com>
2018-02-07 14:10:38 +0100

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('first_name')->nullable();
+            $table->string('name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('image')->nullable();
             $table->jsonb('location')->nullable();
