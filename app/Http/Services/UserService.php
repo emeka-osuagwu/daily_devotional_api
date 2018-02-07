580a5bfc9741840a5eec65f602913b39068b0492
Emeka <emekaosuagwuandela@gmail.com>
2018-02-07 12:24:10 +0100

    update

--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -36,6 +36,8 @@ class UserService
 				'oauth' => $data['oauth'],
 				'image' => $data['image'],
 				'name' => $data['name'],
+				'first_name' => $data['first_name'],
+				'last_name' => $data['last_name'],
 				'password' => bcrypt($data['oauth']),
 				'platform_name' => $data['platform_name'],
 				'account_type' => $data['account_type'],
